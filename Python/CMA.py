234483c48e9124b9c78ed13a96ed7e864923ae37
Daniel <danielderoos99@gmail.com>
2023-01-16 06:13:19 -0500

    Something

--- Python/CMA.py
+++ Python/CMA.py
@@ -9,3 +9,11 @@ class Phonebook:
 
 	def print_contacts(self):
 		print(self.data)
+
+	def add_contact(self):
+		x = input()
+		self.data.update({x:x})
+
+	def delete_contact(self):
+		x = input()
+		self.data.pop({x:x})
