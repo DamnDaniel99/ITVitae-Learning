165387913af3dda57585889d209284d0de7f97d1
Daniel <danielderoos99@gmail.com>
2023-01-16 09:31:22 -0500

    Updated CMA

--- Python/CMA.py
+++ Python/CMA.py
@@ -3,17 +3,30 @@ Simple Contact managing app.
 """
 
 class Phonebook:
+	'''DOC'''
 
 	def __init__(self):
+		'''Initialization.'''
 		self.data = {}
 
 	def print_contacts(self):
+		'''Prints the contact in a dictionairy.'''
 		print(self.data)
 
 	def add_contact(self):
-		x = input().lower()
+		'''Gives the option to add contacts to the dictionairy.'''
+		x = input('''Please insert the contact name: ''').lower()
 		self.data.update({x:x})
 
 	def delete_contact(self):
-		x = input().lower()
+		'''Gives the option to remove contacts from the dictionairy'''
+		x = input('''Please insert the contact name: ''').lower()
 		self.data.pop(x)
+		print('''Contact Deleted!''')
+		print(self.data)
+
+	def search_contact(self):
+		'''Gives the option to search contact in the dictionairy.'''
+		x = input('''Please insert the contact name: ''').lower()
+		result = self.data.get(x)
+		print('''The contact is: ''', result)
